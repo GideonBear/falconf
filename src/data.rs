6f2663574d14b7e66f39dd7d7cfc94aa69bf7c51
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-03-23 17:30:45 +0100

    Add `MachineData`

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
 use crate::full_piece::FullPiece;
-use crate::machine::Machine;
+use crate::machine::{Machine, MachineData};
 
 struct Data {
     pieces: Vec<FullPiece>,
-    machines: Vec<Machine>,
+    machines: HashMap<Machine, MachineData>,
 }
--- src/machine.rs
+++ src/machine.rs
@@ -1,4 +1,9 @@
+// TODO: contains a unique identifier that is generated on first boot and stored in some config file
 #[derive(Debug, Clone)]
-pub(crate) struct Machine {
-    // TODO: contains a unique identifier that is generated on first boot and stored in some config file
+pub(crate) struct Machine(());
+
+
+#[derive(Debug, Clone)]
+pub(crate) struct MachineData {
+    hostname: String,
 }
