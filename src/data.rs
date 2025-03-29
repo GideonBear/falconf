bbcc9c7b4a9e9d7e69c48933df359eddc069374c
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-03-29 17:15:02 +0100

    Add Data serialisation

--- src/data.rs
+++ src/data.rs
@@ -1,7 +1,9 @@
 use crate::full_piece::FullPiece;
 use crate::machine::{Machine, MachineData};
 use std::collections::HashMap;
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Deserialize, Serialize)]
 struct Data {
     pieces: Vec<FullPiece>,
     machines: HashMap<Machine, MachineData>,
--- src/machine.rs
+++ src/machine.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, Hash)]
 pub(crate) struct Machine(Uuid);
 
 impl Machine {
