3e45cc550c61be9dfa1dc9fc03ee81e4a807c6d1
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-03-29 16:41:19 +0100

    Add UUID to machine

--- src/machine.rs
+++ src/machine.rs
@@ -1,9 +1,17 @@
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
 // TODO: contains a unique identifier that is generated on first boot and stored in some config file
-#[derive(Debug, Clone)]
-pub(crate) struct Machine(());
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct Machine(Uuid);
 
+impl Machine {
+    pub(crate) fn new() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub(crate) struct MachineData {
     hostname: String,
 }
