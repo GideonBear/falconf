5ec7c8e1b5ecf00c716ebd82a291ff66cd647853
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-04-24 12:47:29 +0200

    Add `AddArgs::piece`

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,14 +1,15 @@
 use crate::cli::add::add;
 use crate::cli::init::init;
 use crate::cli::sync::sync;
-use clap::{Args, Parser, Subcommand};
+use clap::ArgAction::SetTrue;
+use clap::{Args, Parser, Subcommand, ValueEnum};
 use color_eyre::Result;
 
 mod add;
 mod init;
 mod sync;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(name = "falconf", author, long_version = crate::VERSION)]
 #[command(about = "TODO description")] // TODO
 #[command(propagate_version = true)]
@@ -19,10 +20,10 @@ struct Cli {
     top_level: TopLevelArgs,
 }
 
-#[derive(Args)]
+#[derive(Args, Debug)]
 struct TopLevelArgs {}
 
-#[derive(Subcommand)]
+#[derive(Subcommand, Debug)]
 enum Commands {
     #[command(after_help = "Intialize a new or existing Falconf repo on this machine")]
     Init(InitArgs),
@@ -34,7 +35,7 @@ enum Commands {
     Add(AddArgs),
 }
 
-#[derive(Args)]
+#[derive(Args, Debug)]
 struct InitArgs {
     /// Create a new repo instead of cloning an existing one
     #[arg(long, short)]
@@ -44,11 +45,57 @@ struct InitArgs {
     remote: String,
 }
 
-#[derive(Args)]
+#[derive(Args, Debug)]
 struct SyncArgs {}
 
-#[derive(Args)]
-struct AddArgs {}
+#[derive(ValueEnum, Copy, Clone, Debug)]
+#[value(rename_all = "kebab-case")]
+enum Piece {
+    /// Executes a command in a shell. Expects a command as value.
+    Command,
+    /// Installs an apt package. Expects a package name as value.
+    Apt,
+    /// Tracks a file. Expects an absolute path as value.
+    File,
+    /// Tracks a manual action. Expects a message for the user (description of the action) as value.
+    Manual,
+}
+
+#[derive(Args, Debug)]
+struct AddArgs {
+    /// Omitting this argument will be interpreted as a `command` piece, but it will be translated
+    /// to another piece whenever possible. For example, `falconf add apt install cowsay`
+    /// will result in the same piece as `falconf add --apt cowsay`.
+    #[arg(long = "piece", num_args = 1, require_equals=true, default_value_ifs=[
+        ("_command", "true", "command"),
+        ("_apt", "true", "apt"),
+        ("_file", "true", "file"),
+        ("_manual", "true", "manual"),
+    ])]
+    pub(crate) piece: Option<Piece>,
+
+    /// Shorthand for `--piece=command`
+    #[arg(long="command", action=SetTrue)]
+    _command: (),
+
+    /// Shorthand for `--piece=apt`
+    #[arg(long="apt", action=SetTrue)]
+    _apt: (),
+
+    /// Shorthand for `--piece=file`
+    #[arg(long="file", action=SetTrue)]
+    _file: (),
+
+    /// Shorthand for `--piece=manual`
+    #[arg(long="manual", action=SetTrue)]
+    _manual: (),
+
+    /// The value of the piece. For example the command, the package, etc.
+    /// Quoting this is optional; both `falconf add apt install cowsay` and
+    /// `falconf add "apt install cowsay"` are allowed.
+    #[arg(trailing_var_arg = true)]
+    value: Vec<String>,
+}
 
 pub fn main() -> Result<()> {
     let cli = Cli::parse();
