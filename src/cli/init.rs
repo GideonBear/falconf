231b8b6ad6035fb37f0b61a26ef597dfb9eb32c6
GideonBear <87426140+GideonBear@users.noreply.github.com>
2025-04-24 19:30:05 +0200

    Clippy

--- src/cli/init.rs
+++ src/cli/init.rs
@@ -1,6 +1,6 @@
 use crate::cli::{InitArgs, TopLevelArgs};
 use color_eyre::Result;
 
-pub fn init(top_level_args: TopLevelArgs, args: InitArgs) -> Result<()> {
+pub fn init(_top_level_args: TopLevelArgs, _args: InitArgs) -> Result<()> {
     todo!()
 }
